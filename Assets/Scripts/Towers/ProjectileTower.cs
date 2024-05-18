2864257e457f04b06dde762d053776b4f7447ad7
Furkan Adalar <110781445+FurkanAdalar@users.noreply.github.com>
2024-05-18 19:20:52 +0300

    Firing Cannonballs

--- Assets/Scripts/Towers/ProjectileTower.cs
+++ Assets/Scripts/Towers/ProjectileTower.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class ProjectileTower : MonoBehaviour
 {
+
+    private Tower theTower;
+
+    public GameObject projectile;
+    public Transform firePoint;
+    public float timeBetweenShots = 1f;
+    private float shotCounter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        theTower = GetComponent<Tower>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        shotCounter -= Time.deltaTime;
+        if(shotCounter <= 0 )
+        {
+            shotCounter = timeBetweenShots;
+
+            Instantiate(projectile, firePoint.position, firePoint.rotation);
+        }
     }
 }
