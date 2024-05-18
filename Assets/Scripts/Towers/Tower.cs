c379d497f02f0d931ae9284d67d40c6b1f065c5e
Furkan Adalar <110781445+FurkanAdalar@users.noreply.github.com>
2024-05-18 18:29:55 +0300

    Towers

---
+++ Assets/Scripts/Towers/Tower.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public float range = 3f;
+    public LayerMask whatIsEnemy; //layer seçmemize yarýyor
+    
+    private Collider[] colliderInRange;
+    public List<EnemyController> enemiesInRange = new List<EnemyController>(); //listede ekleme çýkarma iþlemleri çok kolay
+
+    private float checkCounter;
+    public float checkTime = .2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkCounter = checkTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkCounter -= Time.deltaTime; //her framede yenilemenin önüne geçerek biraz daha performasý geliþtiriyoruz
+        if (checkCounter <= 0)
+        {
+            checkCounter = checkTime;
+
+            colliderInRange = Physics.OverlapSphere(transform.position, range, whatIsEnemy);
+
+            enemiesInRange.Clear();
+            foreach (Collider col in colliderInRange)
+            {
+                enemiesInRange.Add(col.GetComponent<EnemyController>());
+            }
+        }
+    }
+}
