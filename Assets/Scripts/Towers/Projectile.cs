dfce9176f403c0e9652eff843bbf9005fd7cf186
Furkan Adalar <110781445+FurkanAdalar@users.noreply.github.com>
2024-05-18 20:48:40 +0300

    Enemies can take damage now

--- Assets/Scripts/Towers/Projectile.cs
+++ Assets/Scripts/Towers/Projectile.cs
@@ -9,6 +9,9 @@ public class Projectile : MonoBehaviour
 
     public GameObject impactEffect;
 
+    public float damageAmount;
+
+    private bool hasDamaged;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,11 @@ public class Projectile : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if(other.tag == "Enemy" && !hasDamaged)
+        {
+            other.GetComponent<EnemyHealthController>().TakeDamage(damageAmount);
+            hasDamaged = true;
+        }
         Instantiate(impactEffect,transform.position,Quaternion.identity); // Quaternion.identity yerine transform.rotation da yazýlabilir
         //yukardaki quaternion.identity bize vector3 deðerini 000 a eþitliyor
         Destroy(gameObject);
