82db7bddce901d1120140708dd1bc80cf7ed5c76
Furkan Adalar <110781445+FurkanAdalar@users.noreply.github.com>
2024-05-18 19:29:01 +0300

    Nearest Target

--- Assets/Scripts/Towers/ProjectileTower.cs
+++ Assets/Scripts/Towers/ProjectileTower.cs
@@ -11,21 +11,47 @@ public class ProjectileTower : MonoBehaviour
     public Transform firePoint;
     public float timeBetweenShots = 1f;
     private float shotCounter;
+
+    private Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        theTower = GetComponent<Tower>();
+        theTower = GetComponent<Tower>(); // tower scriptine eriþmemize yarýyor
     }
 
     // Update is called once per frame
     void Update()
     {
         shotCounter -= Time.deltaTime;
-        if(shotCounter <= 0 )
+        if(shotCounter <= 0 && target != null)
         {
             shotCounter = timeBetweenShots;
 
+            firePoint.LookAt(target);
+
             Instantiate(projectile, firePoint.position, firePoint.rotation);
+        }
+
+        if(theTower.enemiesInRange.Count > 0 )
+        {
+            float minDistance = theTower.range + 1f; // min distance deðerini setledik
+            foreach(EnemyController enemy in theTower.enemiesInRange)
+            {
+                if(enemy != null)
+                {
+                    float distance = Vector3.Distance(transform.position, enemy.transform.position);
+                    if(distance < minDistance)
+                    {
+                        minDistance = distance; //en yakýndaki enemy
+                        target = enemy.transform;// ayný þekilde transformuna
+                    }
+                }
+            }
+        }
+        else
+        {
+            target = null;
         }
     }
 }
